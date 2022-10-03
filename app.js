a68833381d17f4fb3c2c9f9ed9c59c8ffb9fed09
antarpreet11 <singhantarpreet11@gmail.com>
2022-10-03 17:50:25 -0400

    restructure

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
-const request = require('request')
 
 const app = express()
+app.use(express.static('frontend'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/frontend/signup.html')
+})
+
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
